252fdfa1d1040c5019801cc1890736951cc86750
Shawn Ren <hu.ren1432@gmail.com>
2026-03-05 02:08:10 -0500

    Session1_ProblemVersion2_Problem 1 Has been added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_2/Problem1.py
@@ -0,0 +1,13 @@
+'''
+Write a function greet_user() that takes in a string name as a parameter and prints "Hello <name>".
+
+def greet_user(name):
+    pass
+Example Input: Michael
+Example Output: Hello Michael
+
+'''
+def greet_user(name):
+    print(f"Hello, {name}")
+    
+greet_user("Michael")
