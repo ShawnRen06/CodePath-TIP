a7275dd28bbee2895d98e9c3199e741d51ad9c43
Shawn Ren <hu.ren1432@gmail.com>
2026-03-07 20:30:54 -0500

    Week1_Session1_Version1_Problem4 added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_1/Problem4.py
@@ -0,0 +1,22 @@
+'''
+Problem 4: Sum of Two Integers
+The following function returns the sum of two integers: a and b.
+
+def sum(a, b):
+    return a + b
+Use the sum() function to calculate the sum of 13 and 27. Then, use the sum() function again to double the calculated sum and 
+print the result to the console.
+
+Note: Do not use any mathematical operators such as +, -, *, or / when solving this problem.
+
+Example Input: 20 and 8
+Example Output: 56
+
+
+'''
+
+def sum(a,b):
+    x = a+b
+    return x*2
+
+print(sum(20,8))
