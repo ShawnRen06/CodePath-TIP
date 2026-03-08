9c333d712e4cb334705a9a715fc70efdcfce8516
Shawn Ren <hu.ren1432@gmail.com>
2026-03-07 20:55:29 -0500

    Week1_Session1_Version1_Problem11 added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_1/Problem11.py
@@ -0,0 +1,21 @@
+'''
+
+Write a function counter() that uses the range function to print numbers between 1 and a given stop value (inclusive).
+
+def counter(stop):
+    pass
+Example Usage: counter(7). Example Output:
+
+1
+2
+3
+4
+5
+6
+7
+
+'''
+
+def counter(stop):
+    for x in range(1,stop+1):
+        print(x)
