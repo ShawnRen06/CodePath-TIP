901a0cac96fecc281358f6f7f7a0b79b73b574f0
Shawn Ren <hu.ren1432@gmail.com>
2026-03-07 20:38:02 -0500

    Week1_Session1_Version1_Problem7 added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_1/Problem7.py
@@ -0,0 +1,32 @@
+'''
+Let's put what we learned in Problems 1-4 all together! Write a function named what_time_is_it() that takes an integer hour as a parameter.
+If hour is 2, the function should return "taco time 🌮".
+If hour is 12, the function should return "peanut butter jelly time 🥪”.
+Otherwise, the function should return "nap time 😴".
+
+Example Usage:
+
+time = what_time_is_it(2)
+print(time)
+time = what_time_is_it(7)
+print(time)
+time = what_time_is_it(12)
+print(time)
+
+'''
+
+def what_time_is_it(hour):
+    if hour == 2:
+        return "taco time 🌮"
+    elif hour == 12:
+        return "peanut butter jelly time 🥪"
+    else:
+        return "nap time 😴"
+
+
+time = what_time_is_it(2)
+print(time)
+time = what_time_is_it(7)
+print(time)
+time = what_time_is_it(12)
+print(time)
