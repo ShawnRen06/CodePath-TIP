374be7f24d753649f79497de5fcaa86401c9b6c2
Shawn Ren <hu.ren1432@gmail.com>
2026-03-07 20:35:58 -0500

    Week1_Session1_Version1_Problem6 added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_1/Problem6.py
@@ -0,0 +1,31 @@
+'''
+
+Write a function classify_age() that takes an integer age as a parameter and returns "child" if the age is less than 18, and returns "adult" otherwise.
+
+Example Usage:
+
+output = classify_age(18)
+print(output)
+output = classify_age(7)
+print(output)
+output = classify_age(50)
+print(output)
+Output:
+
+adult
+child
+adult
+
+'''
+def classify_age(age):
+    if age < 18:
+        return "child"
+    else:
+        return "adult"
+
+output = classify_age(18)
+print(output)
+output = classify_age(7)
+print(output)
+output = classify_age(50)
+print(output)
