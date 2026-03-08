2b92b327087411504e29a53179da819d74beaded
Shawn Ren <hu.ren1432@gmail.com>
2026-03-07 20:44:46 -0500

    Week1_Session1_Version1_Problem9 added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_1/Problem9.py
@@ -0,0 +1,15 @@
+'''
+Write a function get_first() that takes in a list as a parameter and returns the first item in the list. 
+Return None if the list is empty.
+
+def get_first(lst):
+    pass
+Example Input: [3,1,6,7,5]
+Example Output: 3
+
+'''
+def get_first(lst):
+    if len(lst) > 1:
+        return lst[0]
+    else:
+        return None
