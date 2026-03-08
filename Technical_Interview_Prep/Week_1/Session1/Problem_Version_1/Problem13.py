79a7e4ccbc4b6651c30ba73bdebf8a4ade00ec5c
Shawn Ren <hu.ren1432@gmail.com>
2026-03-07 21:04:58 -0500

    Week1_Session1_Version1_Problem13 added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_1/Problem13.py
@@ -0,0 +1,19 @@
+'''
+
+Problem 13: Total Sum
+Write a function sum_positive_range() that returns the sum of numbers from 1 to a given stop value (inclusive).
+
+def sum_positive_range(stop):
+    pass
+Example Usage: sum = sum_positive_range(6)
+Example Result: sum = 21
+'''
+
+def sum_positive_range(stop):
+    total = 0
+    for i in range(1,stop+1):
+        total += i
+    return total
+
+sum = sum_positive_range(6)
+print(sum)
