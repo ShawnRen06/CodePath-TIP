cb154dd7b127e45fc6a3b86d292c0e569b2255e4
Shawn Ren <hu.ren1432@gmail.com>
2026-03-07 22:24:46 -0500

    Week1_Session1_Version2_Problem02 added

---
+++ Technical_Interview_Prep/Week_1/Session1/Problem_Version_2/Problem02.py
@@ -0,0 +1,16 @@
+'''
+Problem 2: Calculate Difference
+Write a function difference() that returns the difference between two integers a and b (b should be subtracted from a).
+
+def difference(a, b):
+    pass
+Example Usage: diff = difference(8, 3)
+Example Output: diff = 5
+
+'''
+
+def difference(a,b):
+    return a-b
+
+diff = difference(8,3)
+print(diff)
